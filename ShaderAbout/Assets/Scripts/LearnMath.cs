36869b498d07320dfdc35217e6b553066f9834d1
Everglow <30494065+DengHaiYang1234@users.noreply.github.com>
2019-04-02 22:23:27 +0800

    2019.4.2
    学数学！

---
+++ ShaderAbout/Assets/Scripts/LearnMath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnMath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Debug.Log("学数学！！！！！！！！！！！！！！");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
